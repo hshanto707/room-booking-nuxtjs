f9976eb02285fa214e5f5eb351b006994d48a3b0
unknown <hshanto707@gmail.com>
2021-12-26 17:20:37 +0600

    db created

---
+++ assets/data.js
@@ -0,0 +1,77 @@
+export const largeCardSections = [
+  {
+    id: 1,
+    title: "Top-rated Rooms In Cox's Bazar",
+    snippet: "User say these are the best rooms to rent if your looking for a safe and beautiful place for your vacation.",
+    cards: [
+      {
+        id: 11,
+        title: "Entire serviced apartment hosted by Sheikh",
+        snippet: "“Hotel Delower Paradise” is a Luxury Family Hotel managed by “The Significant Ltd.” Situated at Hotel Motel Zone, Kalatoli Road Sugondha point block -A, Cox’s Bazar. It is very close to the world’s longest sea-beach.",
+        image: "https://a0.muscache.com/im/pictures/dc1edcb0-f38e-4bef-91f1-dcc6bfdf87f8.jpg?im_w=1200"
+      },
+      {
+        id: 12,
+        title: "Private room in Cox's Bazar",
+        snippet: "This property is located in the center of the city. It is close to everything. The beach is 7-10 minutes away by rikshaw. This room is a part of a big building and there is always someone around to help.",
+        image: "https://a0.muscache.com/im/pictures/34b2d213-e103-4979-b11f-5cb5403f6c11.jpg?im_w=1200"
+      },
+      {
+        id: 13,
+        title: "Private room in serviced apartment hosted by JatraRooms",
+        snippet: "This is a fully furnished luxurious room with a chic contemporary flair. The room houses one queen size bed and an attached bathroom. The room comes with a Complimentary Breakfast, WiFi, Safety locker, Smart TV Service.",
+        image: "https://a0.muscache.com/im/pictures/9a93559f-d0e4-4455-9710-7c176d2df0d7.jpg?im_w=1200"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Top-rated Rooms In Thailand",
+    snippet: "Have the best vacation in your life with these rooms in Bangkok.",
+    cards: [
+      {
+        id: 21,
+        title: "Entire rental unit hosted by Max",
+        snippet: "Vintage design meets contemporary Bangkok in one of Asoke's most prestigious area. You will find this Condo relaxing retreat after a day of exploring the city. With the rooftop pool and panorama view.",
+        image: "https://a0.muscache.com/im/pictures/75dabd05-95df-4c4e-affc-73169dec18c7.jpg?im_w=1200"
+      },
+      {
+        id: 22,
+        title: "Entire rental unit hosted by Kevin",
+        snippet: "SUNSET PENTHOUSE, beautifully designed to cater your perfect stay in Bangkok. 150 sqm, 2 bedroom, 3 bathroom, spacious, well-lit is your best choice to stay in Bangkok. seven eleven and lotus express within 5 minute walk of the brand new apartment building.",
+        image: "https://a0.muscache.com/im/pictures/255296be-782c-46fe-af1e-377e393760ad.jpg?im_w=1200"
+      },
+      {
+        id: 23,
+        title: "Entire condominium (condo) hosted by Yefeng",
+        snippet: "The room is located in a very good location since all you may need for Bangkok living, just one step away from here. The popular place near BTS Asoke Station & MRT Sukhumvit Station, Korean Town, Lavana Spa & Massage are all within 5-10 mins walking distance.",
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-48138317/original/bf086ecd-9fe6-4265-9dd6-cfc7babdf470.jpeg?im_w=1200"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Top-rated Rooms In Singapore",
+    snippet: "Take down that fire lavishly and in style with our luxury extinguishers.",
+    cards: [
+      {
+        id: 31,
+        title: "Entire rental unit hosted by Kendra",
+        snippet: "A brand new apartment with a sea view located in the heart of the CBD with a prestigious address. It is close to Tanjong Pagar MRT, Marina Bay MRT and Raffles Place MRT. Yoga and Pilates classes available on request.",
+        image: "https://a0.muscache.com/im/pictures/78116f86-cd3f-496a-ab69-4a996eeba146.jpg?im_w=1200"
+      },
+      {
+        id: 32,
+        title: "Tiny house hosted by Lorraine",
+        snippet: "It is located inside Haw Par Villa and is only a mere 5 minutes' walk from Haw Par Villa MRT train station. It comes with a bedroom, living room, dining room, kitchen and bathroom. It has 2 queen size beds that folds into the wall.",
+        image: "https://a0.muscache.com/im/pictures/1b15fbfa-c3ae-41e7-885e-4b28daec2d71.jpg?im_w=1200"
+      },
+      {
+        id: 33,
+        title: "Entire condominium (condo) hosted by Wei",
+        snippet: "Seamless check in, quiet neighbours and good privacy in the apartment. Free parking on visitors' parking lots. Short 5-8 mins walk to the Train station and paya lebar square mall and PLQ.",
+        image: "https://a0.muscache.com/im/pictures/ab6f0cd7-4883-496c-b8fe-73c7e1c8a240.jpg?im_w=1200"
+      }
+    ]
+  }
+]
